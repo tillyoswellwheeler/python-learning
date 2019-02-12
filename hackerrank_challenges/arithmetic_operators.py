5b255746c1ccc4cc38850ffa07ff845f4d09cfc0
Tilly Oswell-Wheeler <tilly.oswellwheeler@hotmail.com>
2019-02-12 18:00:53 +0000

    hackerrank challenges update

--- hackerrank_challenges/arithmetic_operators.py
+++ hackerrank_challenges/arithmetic_operators.py
@@ -13,3 +13,18 @@
 # INPUT TYPE
 #--- The first line contains the first integer, a . The second line contains the second integer, b .
 
+def sum_numbers(a, b):
+    result = a + b
+    return result
+
+def subtract_numbers(a, b):
+    result = a - b
+    return result
+
+def multiply_numbers(a, b):
+    result = a * b
+    return result
+
+print(sum_numbers(2, 4))
+print(subtract_numbers(3, 4))
+print(multiply_numbers(2, 3))
---
+++ hackerrank_challenges/division.py
@@ -0,0 +1,14 @@
+#----------------------------------------------
+# HackerRank Challenges -- Division
+#----------------------------------------------
+
+# ------------------------------------
+# Task
+
+# Read two integers and print two lines. The first line should contain integer division,  a//b .
+# The second line should contain float division, a/b .
+#
+# You don't need to perform any rounding or formatting operations.
+
+# INPUT TYPE
+#--- The first line contains the first integer, a . The second line contains the second integer, b .
