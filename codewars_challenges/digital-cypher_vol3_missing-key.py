e78de5baa1c12fc5e22dbcf76bc07c0f8eda2c26
Tilly Oswell-Wheeler <tilly.oswellwheeler@hotmail.com>
2019-02-16 16:13:10 +0000

    Update to the CodeWar digital cypher challenge--working until key pattern part

--- codewars_challenges/digital-cypher_vol3_missing-key.py
+++ codewars_challenges/digital-cypher_vol3_missing-key.py
@@ -15,41 +15,50 @@
 # --->
 
 import numpy as np
+
 # import re
 
 digital_cypher = {
-        "a": 1, "b": 2, "c": 3, "d": 4, "e": 5, "f": 6, "g": 7, "h": 8, "i": 9, "j": 10, "k": 11, "l": 12, "m": 13,
-        "n": 14, "o": 15, "p": 16, "q": 17, "r": 18, "s": 19, "t": 20, "u": 21, "v": 22, "w": 23, "x": 24, "y": 25,
-        "z": 26
-    }
+    "a": 1, "b": 2, "c": 3, "d": 4, "e": 5, "f": 6, "g": 7, "h": 8, "i": 9, "j": 10, "k": 11, "l": 12, "m": 13,
+    "n": 14, "o": 15, "p": 16, "q": 17, "r": 18, "s": 19, "t": 20, "u": 21, "v": 22, "w": 23, "x": 24, "y": 25,
+    "z": 26
+}
 
 
-def user_string_to_list(): # Change the user string to a list so that you can use the list to search the dict
+def user_string_to_list():  # Change the user string to a list so that you can use the list to search the dict
     user_string = input("What word do you want to encrypt? ").lower()
     string_list = list(user_string)
     return string_list
 
 
-def check_list_against_dict(): # create a list of letters passed through the dictionary
+def check_list_against_dict():  # create a list of letters passed through the dictionary
     string_list = user_string_to_list()
     out = [digital_cypher[letter] for letter in string_list]
-    return out # The output is the code
+    return out  # The output is the code
 
 
 def difference_code_dict():
-    out = check_list_against_dict()
-    int_list = user_input_code()
-    result = np.array([out, int_list])
-    print(result)
+    out = list(check_list_against_dict())  # New variable with words code
+    int_list = user_input_code()  # New variable with the user input code
+    arrays = []  # Adding the two lists to an array
+    arrays.append(out)
+    arrays.append(int_list)
+    array_mat = np.array(arrays)  # Convert the list of lists to an array
+    difference = np.subtract(array_mat[0], array_mat[1])
+    positive_result = abs(difference)  # Converting to positive ints
+    print(type(positive_result))
+
 
 def user_input_code():
     input_code = input("What code do you want to use to encrypt it? **hint** separate with space: ")
-    int_list = input_code.split()
-    print(int_list)
-    return int_list
+    str_list = input_code.split()  # This turns the str into a list
+    ints_code_list = [int(i) for i in str_list]  # This loops through the list and changes each item to an int.
+    return ints_code_list
+
 
 difference_code_dict()
-
+#
+# print(check_list_against_dict())
 
 
 # # using ''.join makes the new list a single string
