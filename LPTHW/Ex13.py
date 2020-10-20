4b73f0d2386248fcd47c92f3fbd3e6b27b4f06de
Matilda Oswell-Wheeler <m-oswell-wheeler@Matildas-MacBook-Pro.local>
2020-10-20 19:04:18 +0100

    Adding in old LPTHW files

---
+++ LPTHW/Ex13.py
@@ -0,0 +1,11 @@
+# ------------------------------------
+# LPTHW  -- Ex13. Parameters, Unpacking, Variables
+# ------------------------------------
+
+from sys import argv
+script, first, second, third = argv
+
+print("The script is called: ", script)
+print("The first variable is: ", first)
+print("The second variable is: ", second)
+print("The third variable is: ", third)
